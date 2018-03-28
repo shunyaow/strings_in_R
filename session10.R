3d8e3a0cc026a2e24ac10c7fed3f2bc5d80376e6
Shunyao Wang <Christina@ShunyaodeMacBook-Air.local>
2018-03-27 20:45:35 -0700

    changes

--- session10.R
+++ session10.R
@@ -59,3 +59,34 @@ bodies=str_sub(emails,
                start=breaks[,2])
 cat(bodies[6])
 
+#second lab
+#1
+fruit=c("apple","banana","pear","pineapple")
+#2
+#detects if the pattern is found
+str_detect(fruit,"a")
+#pattern starts with a
+str_detect(fruit,"^a")
+str_detect(fruit,"a$")
+
+#pattern to check if it has a or e or i or o or u
+str_detect(fruit,"[aeiou]")
+#a or b or c or d
+str_detect(fruit,"[a-d]")
+#if it has a number
+str_detect(fruit,"[0-9]")
+
+#3
+str_detect(fruit,"^a[a-z]*e$")
+#. could be any character or number
+str_detect(fruit,"^a.e$")
+
+#4
+phone=c("213 740 4826",
+       "213-740-4826",
+       "(213) 740 4826")
+str_detect(phone,"[(]?[0-9]{3}[)]?[ -] [0-9]{3}[ -][0-9]{4}")
+
+#5
+
+
